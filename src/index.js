5a40da78129d6f8993c4d9e2c4d3ca158a0dcdae
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-12-21 22:40:59 +0530

    feat: complete the api

--- src/index.js
+++ src/index.js
@@ -1,43 +1,68 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const Subscription = require('./resolvers/Subscription');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Vote = require('./resolvers/Vote');
 const fs = require('fs');
 const path = require('path');
-const { PrismaClient } = require('@prisma/client')
-const { ApolloServer } = require('apollo-server');
+const { getUserId } = require('./utils');
 
-const prisma = new PrismaClient();
+const pubsub = new PubSub();
+
+const prisma = new PrismaClient({
+  errorFormat: 'minimal'
+});
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: async (parent, args, context) => {
-            return context.prisma.link.findMany()
-        },
-    },
-    Mutation: {
-        post: (parent, args, context, info) => {
-            const newLink = context.prisma.link.create({
-                data: {
-                    url: args.url,
-                    description: args.description,
-                },
-            })
-            return newLink
-        },
-    },
-}
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
+};
 
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync(
-        path.join(__dirname, 'schema.graphql'),
-        'utf8'
-    ),
-    resolvers,
-    context: {
-        prisma
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  },
+  subscriptions: {
+    onConnect: (connectionParams) => {
+      if (connectionParams.authToken) {
+        return {
+          prisma,
+          userId: getUserId(
+            null,
+            connectionParams.authToken
+          )
+        };
+      } else {
+        return {
+          prisma
+        };
+      }
     }
-})
+  }
+});
 
 server
-    .listen()
-    .then(({ url }) =>
-        console.log(`Server is running on ${url}`)
-    );
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
